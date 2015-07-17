5676f4ca1c99e556f9cda6f2816b09b7fb66a5c8
Dmitriy Tarasov <info@rubysamurai.com>
2015-07-17 17:25:47 +0300

    Fix for consistency between supported Ruby and Rails versions

--- spec/helpers/seo_meta_description_helper_spec.rb
+++ spec/helpers/seo_meta_description_helper_spec.rb
@@ -10,15 +10,14 @@ RSpec.describe SeoRailsHelpers::SeoMetaDescriptionHelper, type: :helper do
     it 'returns meta description for default description option' do
       allow(helper).to receive(:content_for).with(:description)
         .and_return('Meta description')
-      expect(helper.seo_meta_description)
-        .to eq '<meta name="description" content="Meta description" />'
+      expect(helper.seo_meta_description).to include 'content="Meta description'
     end
 
     it 'returns meta description for custom description option' do
       allow(helper).to receive(:content_for).with(:meta_description)
         .and_return('Meta description')
       expect(helper.seo_meta_description(description: :meta_description))
-        .to eq '<meta name="description" content="Meta description" />'
+        .to include 'content="Meta description'
     end
   end
 end
